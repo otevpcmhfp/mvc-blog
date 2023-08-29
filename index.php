2ce7ceb330ab020f6991bebea8b16053a6588e8a
Kyle Murphy <kyle@kylemurphy.io>
2023-08-29 23:53:30 +1000

    Removed comments

--- index.php
+++ index.php
@@ -1,21 +1,14 @@
 <?php
 require 'Database.php';
-//require 'models/db.php';
-
-//$db = new Database();
 
 // Get the requested path
 $route = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-//die($route);
 
 // Our list of routes
 $routes = [
     '/' => 'controllers/HomeController.php',
     '/blog' => 'controllers/BlogController.php',
 ];
-
-//die(var_dump("hello"));
 
 // Check our routes map to see if we have a place to go
 if(array_key_exists($route, $routes)) {
